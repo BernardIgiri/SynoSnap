c4255aa637c90572ff8ee9169a5658e44d48a222
bernard <bernard.igiri@gmail.com>
2016-08-11 10:28:08 -0400

    Wrote DryRun and Live file handlers. Restructured code.

--- synosnap.py
+++ synosnap.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-import os
-import yaml
-import sys
+import os, yaml, sys, shutil, datetime
 
 def main(arguments):
 	if len(arguments) < 2:
@@ -17,6 +15,41 @@ def main(arguments):
 	return
 
 def updateSnapShot(timePeriod, nSnapshots, isDryRun):
+	if isDryRun:
+		fileHandler = DryRunFileHandler()
+	else:
+		fileHandler = LiveFileHandler()
 	return
+
+class FileHandlerBase:
+	def ifFolderExists(folder):
+		return os.path.exists(folder)
+	def getTimeStamp(folder):
+		return os.stat(folder).st_mtime
+
+class DryRunFileHandler(FileHandlerBase):
+	def deleteFolder(folder):
+		print "Delete " + folder
+	def moveFolder(folderA, folderB):
+		print "Move " + folderA + " to " + folderB;
+	def copyFolder(folderA, folderB):
+		print "Copy " + folderA + " to " + folderB;
+	def touchFolder(folder):
+		return
+
+class LiveFileHandler(FileHandlerBase):
+	def _init_(self):
+		self.logger = DryRunFileHandler()
+	def deleteFolder(folder):
+		self.logger.deleteFolder(folder)
+		shutil.rmtree(folder)
+	def moveFolder(folderA, folderB):
+		self.logger.moveFolder(folderA, folderB)
+		shutil.move(folderA, folderB)
+	def copyFolder(folderA, folderB):
+		self.logger.copyFolder(folderA, folderB)
+		os.system('cp -al "' + folderA + '" "' + folderB + '"')
+	def touchFolder(folder):
+		os.system('touch "' + folder + "'")
 
 main(sys.argv)
