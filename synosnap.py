ec8e4005449924d2ddb3091e7a4bbd76a64cf7dc
bernard <bernard.igiri@gmail.com>
2016-08-20 18:34:56 -0400

    Made SynoSnap path agnostic.

--- synosnap.py
+++ synosnap.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import sys, os
-sys.path.insert(0, os.path.abspath('src'))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), 'src'))
 import main
 
 main.main(sys.argv)
